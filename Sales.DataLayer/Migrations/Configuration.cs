4ac708e1bfa79ee7d2a8ce8b095aef09bbfda593
t4rn <kwitkowski.it@gmail.com>
2017-10-11 10:16:45 +0200

    Cleaning unused files

--- Sales.DataLayer/Migrations/Configuration.cs
+++ Sales.DataLayer/Migrations/Configuration.cs
@@ -21,13 +21,23 @@ namespace Sales.DataLayer.Migrations
                     PONumber = "123",
                     SalesOrderItems =
                     {
-                        new SalesOrderItem { ProductCode = "OnePlus 3T", Quantity = 2, UnitPrice = 2000.33M },
-                        new SalesOrderItem { ProductCode = "Samsung S8", Quantity = 1, UnitPrice = 3000.55M },
-                        new SalesOrderItem { ProductCode = "Nokia 3310", Quantity = 7, UnitPrice = 200.59M },
+                        new SalesOrderItem { ProductCode = "OnePlus", Quantity = 2, UnitPrice = 2000.33M },
+                        new SalesOrderItem { ProductCode = "Samsung", Quantity = 1, UnitPrice = 3000.55M },
+                        new SalesOrderItem { ProductCode = "Nokia", Quantity = 7, UnitPrice = 200.59M },
                     },
                 },
                 new SalesOrder { CustomerName = "John", PONumber = "234" },
-                new SalesOrder { CustomerName = "Anna", PONumber = "456" }
+                new SalesOrder
+                {
+                    CustomerName = "Anna",
+                    PONumber = "456",
+                    SalesOrderItems =
+                    {
+                        new SalesOrderItem { ProductCode = "ABC", Quantity = 12, UnitPrice = 3500.33M },
+                        new SalesOrderItem { ProductCode = "XYZ", Quantity = 5, UnitPrice = 32300.55M },
+                        new SalesOrderItem { ProductCode = "RET", Quantity = 8, UnitPrice = 670.59M },
+                    },
+                }
                 );
         }
     }
--- Sales.Web/Startup.cs
+++ Sales.Web/Startup.cs
@@ -8,7 +8,6 @@ namespace Sales.Web
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
